70a62af04cbb251549287b0a8d44f8049a8c1594
ahmedammarsd <ahmed091999287@gmail.com>
2024-04-22 16:03:43 +0200

    add images bank and file colors

---
+++ lib/colors/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+Color kWhiteOne = const Color(0xffFFFFFF);
+Color kWhiteTwo = const Color(0xffF1F1F1);
+Color kDarkOne = const Color(0xff303745);
+Color kDarkTwo = const Color(0xff262831);
+Color kDarkThree = const Color(0xff292D3A);
+Color kBlack = const Color(0xff000000);
